0a3da7d8d5e1e717ce9dd62a578174983ca64d85
devsenacvinicius <dev.senac.vinicius@gmail.com>
2026-05-07 11:55:34 -0300

    Atualizando TASKDEV

--- js/main.js
+++ js/main.js
@@ -1,1 +1,16 @@
-console.log("aplicação Gernciador de tarefas")
+import {
+    obterTextoTarefa,
+    limparInput,
+    adicionarTarefaNalista,
+} from "./dom.js";
+
+const form = document.querySelector("#form-tarefa");
+
+form.addEventListener("submit", function (event) {
+    event.preventDefault();
+
+    const texto = obterTextoTarefa();
+
+    adicionarTarefaNalista(texto);
+    limparInput();
+});
