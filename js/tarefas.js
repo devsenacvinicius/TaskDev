8a586b7b5e6364d2b161079d4941c03ac3ec34e7
devsenacvinicius <dev.senac.vinicius@gmail.com>
2026-05-12 10:33:37 -0300

    Aula 04 e 05

--- js/tarefas.js
+++ js/tarefas.js
@@ -1,1 +1,22 @@
-// tarefas.js
+// Este módulo é responsável por validar as terefas antes de adiciona-las
+
+// Função para validar o texto da tarefa, tem que ter ao menos 3 caracteres
+export function validarTarefa(texto) {
+    if (texto.trim() === "") {
+        return {
+            valida: false,
+            mensagem: "A tarefa não poda estar vazia.",
+            };
+        };
+            if(texto.length < 3) {
+            return {
+            valida: false,
+            mensagem: "A tarefa deve ter ao menos 3 caracteres.",
+     };           
+   }
+    
+    return {
+        valida: true,
+        mensagem: "Tarefa válida.",
+    };
+}
